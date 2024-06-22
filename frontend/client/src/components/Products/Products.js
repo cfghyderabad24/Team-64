6a0b288dab02357accfc5a5cd66a931bddd3276b
pranay65 <family.yerram@gmail.com>
2024-06-23 02:39:39 +0530

    Added products page

--- frontend/client/src/components/Products/Products.js
+++ frontend/client/src/components/Products/Products.js
@@ -5,10 +5,20 @@ import "./products.css";
 
 function Products() {
   const [data, setData] = useState([]);
+  const [price, setPrice] = useState(0);
 
   const loadData = async () => {
     const res = await Axios.get("http://localhost:7500/products");
     setData(res.data.payload);
+  };
+
+  const handleUpdate = (cost) => {
+    console.log(cost);
+    if (cost > 0) {
+      setPrice(price + cost);
+    } else if (price - cost >= 0) {
+      setPrice(price + cost);
+    }
   };
 
   useEffect(() => {
@@ -16,11 +26,14 @@ function Products() {
   }, []);
 
   return (
-    <div className="products-cont">
-      {data.map((i) => {
-        return <Product item={i} />;
-      })}
-    </div>
+    <>
+      <div className="products-cont">
+        {data.map((i) => {
+          return <Product item={i} onUpdate={handleUpdate} />;
+        })}
+      </div>
+      <h2>Your Subtotal: ₹{price}</h2>
+    </>
   );
 }
 
