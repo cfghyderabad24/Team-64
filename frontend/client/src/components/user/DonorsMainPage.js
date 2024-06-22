476ed818763414c40aab9c12f6712a300a56e526
aashritharaj26 <141063242+aashritharaj26@users.noreply.github.com>
2024-06-23 00:02:52 +0530

    added admin and user pages, donor pages

--- frontend/client/src/components/user/DonorsMainPage.js
+++ frontend/client/src/components/user/DonorsMainPage.js
@@ -1,23 +1,44 @@
-import { Button } from 'bootstrap'
-import React from 'react'
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 function DonorsMainPage() {
-
   return (
-    <div>
-        <div className='row'>
-        <div className='column col-1 card'>
-        <button className="btn ">Buy Products</button>
+    <div className="container my-5">
+      <div className="text-center mb-4">
+        <h1>Welcome to Donor's Hub</h1>
+        <p>Make a difference by buying, donating products or helping a child in need.</p>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4 mb-3">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <button className="btn btn-primary btn-lg w-100">Buy Products
+              <Link to="/buy-products" className='p-4'>Buy Products</Link>
+              </button>
+            </div>
+          </div>
         </div>
-        <div className='column col-1 card'>
-        <button className="btn ">Donate Products</button>
+        <div className="col-md-4 mb-3">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <button className="btn btn-success btn-lg w-100">
+              <Link to="/donate-products" className='p-4'>Donate Products</Link>
+              </button>
+            </div>
+          </div>
         </div>
-        <div className='column col-1 card'>
-        <button className="btn ">Donate For A Child</button>
+        <div className="col-md-4 mb-3">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <button className="btn btn-warning btn-lg w-100">
+              <Link to="/donate-fr-child" className='p-4'>Donate For A Child</Link>
+              </button>
+            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DonorsMainPage
+export default DonorsMainPage;
--- frontend/client/src/components/user/Header.js
+++ frontend/client/src/components/user/Header.js
@@ -12,17 +12,57 @@ export default function Header() {
         }
   return (
     <div>
-        <div className=''>
-            <div>
-                <ul className='mt-3'>
-                    <Link to="/" className='p-4' >Home</Link>
-                    <Link to="/donor-page" className='p-4'>Donors main page</Link>
-                </ul>
+       <nav className="navbar sticky-top navbar-expand-lg navbar-dark">
+            <div className="container">
+              <img className='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXu5h3InXUDGTEkvdTl9X756shfsYMicPyhQ&s' style={{'width':'100px'}}></img>
+                <a className="navbar-brand grow" href="/">Good Universe</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item">
+                        <Link to="/home" className='p-4' style={{'text-decoration':'none'}}>Home</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownLost" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                What We Do
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownLost">
+                                <a className="dropdown-item" href="/">Climate change</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/">Health & Well Being</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="/">Gender Equality</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Products</a>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/contact" style={{'text-decoration':'none'}} className='p-4'>Contact</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownLost" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Profile
+                            </a>
+                           
+                            
+                        </li>
+                        <li>
+                        <button className='' onClick={handleLogOut}> Sign Out </button></li>
+                           <li>
+                           <Link to="/donors-page" style={{'text-decoration':'none'}}>Donors Page</Link></li>
+
+                    </ul>
+                </div>
             </div>
-            <div>
-            <button className='' onClick={handleLogOut}> Sign Out </button>
-            </div>
-        </div>
+        </nav>
         <hr className=''/>
 
         <Routes>
